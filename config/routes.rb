3b84cf6d721c779e3451524398fced8e77cad8a3
Julio Gama <juliogama@live.com>
2018-04-27 02:26:32 +0000

    Config the routes file for the api namespace

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,12 @@
 Rails.application.routes.draw do
   
-  namespace :api, defaults: { format: :json } do
+  namespace :api,
+            defaults: { format: :json }, 
+            constraints: { subdomain: 'api' },
+            path: '/' 
+  do
 
+    
   end
   
 end
